ccd3261faa2beda7ecb37ccaac85155f99efa1b7
Bea Gilfix <149093312+Bea7034@users.noreply.github.com>
2024-08-25 18:09:26 -0700

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,24 +1,53 @@
-import ezdxf
+#Blot border dimensions
+WIDTH = 125
+HEIGHT = 125
+export_count = 0
+
+def log(obj,iteration,coordinates,entity_recognized):
+    if entity_recognized:
+        print("Processing entity "+str(iteration)+", Entity = "+obj+ " @ coordinates = "+str(coordinates[0])+","+str(coordinates[1]))
+    else:
+        print("Attempted to process unrecognized entity at iteration "+str(iteration)+", skipping")
+
+
+def negate_negativity(float):
+    if (0>float):
+        return -float
 
 def dxf_to_blot_code(dxf_filename,scale):
-    print("Function ran with filename "+dxf_filename)
+    print("\nFunction ran with filename "+dxf_filename+"\n")
     file = ezdxf.readfile(dxf_filename)
     blot_code = [] #Blot code gets compiled here
 
     dxfmodel = file.modelspace() #Readability fun :)
+    i = 0
+    center_x = WIDTH / 2
+    center_y = HEIGHT / 2
 
-    for entity in dxfmodel: #Pull every object from the model
+    for entity in dxfmodel: #Pull every entity from the model
+        #TODO: Figure out why structured polygons aren't being represented as lines
+        i += 1
         if entity.dxftype() == 'LINE':
+
             start = entity.dxf.start
             end = entity.dxf.end
-            polyline = [ #Define a STRAIGHT line by registering the coordinates of its start and endpoint
-                [round(start.x * scale, 2), round(start.y * scale, 2)],
-                [round(end.x * scale, 2), round(end.y * scale, 2)]
-            ]
-            blot_code.append(f'finalLines.push({polyline});')
-            #An example polyline should look like 0.9999999999999998, 0.9999999999999998 uhh I gotta work on scale...
 
+            startingpoint = [negate_negativity((round(start.x * scale - center_x, 2))), 
+                             negate_negativity((round(start.y * scale - center_y, 2)))]
+
+            endingpoint = [negate_negativity((round(end.x * scale - center_x, 2))), 
+                           negate_negativity((round(end.y * scale - center_y, 2)))]
+
+            polyline = [ #Define a STRAIGHT line by registering the coordinates of its start and endpoint
+                startingpoint,
+                endingpoint
+            ]
+            log("line",i,[startingpoint,endingpoint],True)
+            blot_code.append(f'finalLines.push({polyline});')
+            #An example polyline should look like [30,30], [-30, -30] uhh I gotta work on scale...
+        log("?",i,["?","?"],False) #If entity is unrecognized
         #todo-work on more entity types
+
 
     return blot_code
 
@@ -28,8 +57,6 @@ def main():
     blot_code = dxf_to_blot_code(filename+".dxf",scale)
     with open('output_blot.txt', 'w') as f:
         f.write("\n".join(blot_code))
-
-main()
 
 if __name__ == "__main__":
     main()
